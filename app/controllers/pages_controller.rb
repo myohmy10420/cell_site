1eb83918bb9b60259177ddf74763c975229320a3
KurtHsu <myohmy10420@gmail.com>
2018-11-20 23:01:57 +0800

    add new_products and pop_products partails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   def welcome
+    @new_products = slice_three_items_a_group(Product.all)
+    @pop_products = slice_three_items_a_group(Product.all)
+  end
 
+  def slice_three_items_a_group(array)
+    groups_array = []
+    array.each_slice(3) do |items|
+      groups_array.push(items)
+    end
+    return groups_array
   end
 end
