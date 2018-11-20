b0dfcc476b0bf085ba328e6798f89579e2ad2f7e
KurtHsu <myohmy10420@gmail.com>
2018-11-20 23:54:45 +0800

    product can add image

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :slogan
   validates_presence_of :content
+
+  has_attached_file :avatar,
+    styles: {
+      large: "800x400>",
+      small: "240x200>"
+    },
+    default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: ["image/jpg", "image/jpeg", "image/gif", "image/png"]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_085754) do
+ActiveRecord::Schema.define(version: 2018_11_20_153717) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2018_11_18_085754) do
     t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
