db532a2282c50aa00fe610e57b921df4a3a23ced
KurtHsu <myohmy10420@gmail.com>
2018-11-25 00:18:36 +0800

    update telecommunication column

--- db/migrate/20181121134221_create_telecommunications.rb
+++ db/migrate/20181121134221_create_telecommunications.rb
@@ -2,10 +2,10 @@ class CreateTelecommunications < ActiveRecord::Migration[5.2]
   def change
     create_table :telecommunications do |t|
       t.string :name
-      t.string :avatar_file_name
-      t.string :avatar_content_type
-      t.integer :avatar_file_size
-      t.datetime :avatar_updated_at
+      t.string :logo_file_name
+      t.string :logo_content_type
+      t.integer :logo_file_size
+      t.datetime :logo_updated_at
 
       t.timestamps
     end
