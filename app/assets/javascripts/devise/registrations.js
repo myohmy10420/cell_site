052fa453934a472b8a3b4466730196bee410ad0e
KurtHsu <myohmy10420@gmail.com>
2019-03-05 22:57:41 +0800

    calculate age when user registrate

--- app/assets/javascripts/devise/registrations.js
+++ app/assets/javascripts/devise/registrations.js
@@ -6,4 +6,15 @@ $(document).ready(function () {
       $(".registrations_btn").prop("disabled", true)
     }
   })
+
+  $(".birthday").on("change", function (event) {
+    var birthArry = event.target.value.split("-")
+    var year = parseInt(birthArry[0])
+    var month = parseInt(birthArry[1]) - 1
+    var day = parseInt(birthArry[2])
+    var birthday = new Date(year, month, day)
+    var ageDifMs = Date.now() - birthday
+    var ageDate = new Date(ageDifMs)
+    $(".age").text(parseInt(ageDate.getUTCFullYear() - 1970))
+  })
 })
