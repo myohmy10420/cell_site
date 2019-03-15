1bd50365f5765280f0775cee60ef80628ffeff24
KurtHsu <myohmy10420@gmail.com>
2019-03-16 00:09:02 +0800

    product add admin_quick_add_images admin_products_search

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -62,6 +62,19 @@ module Admin
       render "index"
     end
 
+    def quick_add_images
+      params[:files].each do |file|
+        extension_dot = file.original_filename.rindex(".")
+        double_dash = file.original_filename.rindex("__")
+        file_name = file.original_filename.slice(0, double_dash || extension_dot)
+
+        product = Product.find_by(name: file_name)
+        product.product_images.create(image: file) if product
+      end if params[:files].presence
+
+      redirect_to admin_products_path
+    end
+
     private
 
     def product_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :carousel_ads, only: [:index, :create, :destroy]
     resources :products
     post "search_products", to: "products#search"
+    post "add/product/images", to: "products#quick_add_images"
     resources :product_images, only: [:destroy]
     resources :brands
     post "search_brands", to: "brands#search"
