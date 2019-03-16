875e125bc7a0a42eba95e0cfc61e6bbed6c467b7
KurtHsu <myohmy10420@gmail.com>
2019-03-16 15:31:17 +0800

    update admin users view and add partial admin_user_search

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,31 @@
+module UsersHelper
+  def render_human_sex(sex)
+    return "-" if !sex
+    sex == "male" ? "男" : "女"
+  end
+
+  def render_roles(roles)
+    return if !roles.presence
+    badge_tags = new_badge_tags_by(roles)
+    badge_tags.join.html_safe
+  end
+
+  def new_badge_tags_by(roles)
+    roles.map do |role|
+      class_name = "badge badge-success"
+      style = "margin-right: 5px"
+      content_tag :span, human_role_name(role.name), class: class_name, style: style
+    end
+  end
+
+  def human_role_name(name)
+    case name
+    when "admin"
+      "管理員"
+    when "normal"
+      "一般會員"
+    else
+      "未知角色"
+    end
+  end
+end
