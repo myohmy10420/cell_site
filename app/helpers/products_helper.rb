d34abd744262b1d2be40ae137be74c15f0e30e4c
KurtHsu <myohmy10420@gmail.com>
2019-03-21 00:46:14 +0800

    fix product_is_searched? helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -54,6 +54,7 @@ module ProductsHelper
   end
 
   def product_is_searched?(product, search_key)
+    return unless search_key.presence
     product.name.downcase.include?(search_key.downcase)
   end
 
