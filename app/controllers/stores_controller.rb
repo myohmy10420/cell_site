d80a60515e85e4e4c662ed07e7c946ffbf3da1a2
KurtHsu <myohmy10420@gmail.com>
2019-03-21 00:41:25 +0800

    add store meta_tags_params

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,17 @@
 class StoresController < ApplicationController
   def index
     @stores = Store.all
+    set_meta_tag
+  end
+
+  private
+
+  def set_meta_tag
+    @page_title = "門市據點"
+    @page_description = "就是便宜門市據點"
+    @page_keywords = ""
+    @stores.each do |store|
+      @page_keywords += "," + store.name
+    end
   end
 end
