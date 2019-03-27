8c5d630f4b47891ff7621e9ec38bba3fc96ea0a3
KurtHsu <myohmy10420@gmail.com>
2019-03-28 00:16:16 +0800

    inhance telecommunications excel import

--- app/controllers/api/v1/excel/telecommunications_controller.rb
+++ app/controllers/api/v1/excel/telecommunications_controller.rb
@@ -1,10 +1,10 @@
 module Api
   module V1
     module Excel
-      class TelecommunicationsController < ApplicationController
-        def export
-          @telecommunications = Telecommunication.all.order('updated_at DESC')
+      class TelecommunicationsController < Admin::BaseController
+        before_action :get_telecommunications
 
+        def export
           respond_to do |format|
             format.xlsx {
               headers["Content-Disposition"] = "attachment; filename=電信.xlsx"
@@ -16,12 +16,30 @@ module Api
           require 'roo'
 
           workbook = Roo::Excelx.new(params[:file].path) if params[:file]
+          @excel_import_errors = ""
           workbook.drop(1).each do |row|
             name = row[0]
-            Telecommunication.create(name: name) if Telecommunication.find_by(name: name).nil?
+
+            if Telecommunication.find_by(name: name).nil?
+              new_telecommunication = Telecommunication.new(name: name)
+              next if new_telecommunication.save
+              @excel_import_errors += new_telecommunication.name + new_telecommunication.errors.full_messages.join(", ") + "<br>"
+            end
           end if workbook
 
-          redirect_to admin_telecommunications_path
+          if @excel_import_errors.presence
+            @excel_import_errors = @excel_import_errors.html_safe
+          else
+            flash[:notice] = "匯入成功！"
+          end
+
+          render "admin/telecommunications/index"
+        end
+
+        private
+
+        def get_telecommunications
+          @telecommunications = Telecommunication.all.order('updated_at DESC')
         end
       end
     end
