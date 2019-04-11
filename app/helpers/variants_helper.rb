c87c7289097517b12859bf2155789866d6ddc963
KurtHsu <myohmy10420@gmail.com>
2019-04-11 23:50:17 +0800

    update variant index view

--- app/helpers/variants_helper.rb
+++ app/helpers/variants_helper.rb
@@ -3,4 +3,12 @@ module VariantsHelper
     telecommunication = Telecommunication.find_by(id: variant.telecommunication_id)
     telecommunication.name || "-"
   end
+
+  def render_variant_discount(discount)
+    if discount.to_i == 0
+      discount || "下殺"
+    else
+      "$" + discount.to_s
+    end
+  end
 end
