e1002dcf52f584f3d64f915e1a9b8a04c2251533
KurtHsu <myohmy10420@gmail.com>
2019-05-08 22:10:09 +0800

    add detect_user_device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
+require "browser"
+
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :detect_user_device
 
   private
 
@@ -8,5 +11,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  def detect_user_device
+    if browser.device.mobile?
+      request.variant = :phone
+    end
   end
 end
