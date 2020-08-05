76ffd323ae040922ff3f5403498ecc0d34275644
KurtHsu <myohmy10420@gmail.com>
2020-08-05 22:16:03 +0800

    add font family

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,6 @@ module CellWeb
     config.load_defaults 5.2
     config.time_zone = "Taipei"
     config.i18n.default_locale = "zh-TW"
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
