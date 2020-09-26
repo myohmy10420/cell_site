ce021db32f22b80961c2ec78fb93311b411717ca
KurtHsu <myohmy10420@gmail.com>
2020-09-26 22:05:35 +0800

    fix pre_expand_brand helper

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    category = Category.find(params[:id])
-    @products = category.products
+    @category = Category.find(params[:id])
+    @products = @category.products
   end
 end
--- app/helpers/side_bar_helper.rb
+++ app/helpers/side_bar_helper.rb
@@ -1,5 +1,5 @@
 module SideBarHelper
-  def pre_expand_product_brand(brand, brand_product)
-    'show' if brand == brand_product
+  def pre_expand_brand(brand)
+    'show' if brand == (@product.try(:brand) || @category.try(:brand))
   end
 end
